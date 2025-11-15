3ab09effdc4bacb872c56e515fa7ffbd43e397ac
vasilkovalyov <vasilkovalyov@gmail.com>
2025-11-15 22:25:30 +0100

    add redirect to home page after logout

--- src/widgets/components/logout-button/logout-button.tsx
+++ src/widgets/components/logout-button/logout-button.tsx
@@ -6,6 +6,8 @@ import { useTranslations } from "next-intl";
 import { Button } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
 
+import { useRouter } from "@/app/routing";
+import { AppRoutes } from "@/src/shared/routes";
 import { useLogoutMutation } from "@/app/store/slices/auth/auth.api";
 
 type LogoutButtonProps = {
@@ -17,9 +19,14 @@ const LogoutButton: FC<LogoutButtonProps> = ({ size, useIcon }) => {
   const t = useTranslations();
   const [logoutApi] = useLogoutMutation();
 
+  const router = useRouter();
+
   function onLogout(): void {
     logoutApi()
       .unwrap()
+      .then(() => {
+        router.push(AppRoutes.home);
+      })
       .catch((e) => {
         console.log(e);
       });
